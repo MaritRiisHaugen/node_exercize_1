7212e0ecfe2af631f773a11c019201388aa2bc59
unknown <maritriishaugen@gmail.com>
2017-10-04 17:05:51 +0200

    inital

---
+++ README.md
@@ -0,0 +1,1 @@
+"# node_exercize_1" 
---
+++ greeter.js
@@ -0,0 +1,5 @@
+var greetings = ["Greetings", "Hello", "Salutations", "Hi", "Good day" ];
+
+var random = Math.floor(Math.random()*(greetings.length+1));
+
+console.log(greetings[random]);
